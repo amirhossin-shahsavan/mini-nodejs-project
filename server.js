85ea6d85d219849396049201e74f2ba8d9dedb4d
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-20 00:34:13 +0330

    add http module

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const http = requier('http')
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req,res)=>{
+
+}).listen(PORT)
+console.log(`server is run ${PORT} http://localhost${PORT} `);
+
+
