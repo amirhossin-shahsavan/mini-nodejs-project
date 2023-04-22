01d4b427b4f8fd1455f39a06d285b97375858d88
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-22 18:15:56 +0330

    add find id functoin

--- model/product.model.js
+++ model/product.model.js
@@ -5,7 +5,14 @@ async function find(){
     })
 }
 
+async function findById(id){
+    return new Promise((resolve,reject)=>{
+        resolve(products.find(product => product.id == id));
+    })
+}
+
 const ProductsModel = {
-    find
+    find,
+    findById
 }
 module.exports = ProductsModel;
