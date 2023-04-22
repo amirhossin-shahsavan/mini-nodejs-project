2e7316ff460d1873fbb83509c3b963a1e514e316
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-22 16:49:34 +0330

    add routs

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const products = require('./data/products.json')
 const PORT = 3000;
 const server = http.createServer((req,res)=>{
-
+    if(req.url=='/api/products'){
+        res.writeHead(200,{'Content-Type':'application/json'}); 
+        res.write(JSON.stringify(products))
+        res.end()
+    }
+    else{
+        res.writeHead(404,{'Content-Type':'application/json'}); 
+        res.write(JSON.stringify({message:'Route Not Find'}))
+        res.end()
+    }
+    
 }).listen(PORT)
 console.log(`server is run ${PORT} http://localhost${PORT} `);
-
 
