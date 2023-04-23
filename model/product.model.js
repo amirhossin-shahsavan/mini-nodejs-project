102b85d0d95fd3467dbe190ad298ebe0e2271013
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-23 19:57:26 +0330

    add update model

--- model/product.model.js
+++ model/product.model.js
@@ -23,9 +23,23 @@ async function Creat(product){
     })
 }
 
+async function Update(id , payload){
+    return new Promise((resolve,reject)=>{
+        products.map(product => {
+            if(product.id == id){Object.assign(product,payload)}
+            return product
+        })
+        fs.writeFile(`${process.cwd()}/data/products.json`, JSON.stringify(products), (err)=>{
+            if(err){reject(err)}
+            else{resolve({message:'product updated'})}
+        })
+    })
+}
+
 const ProductsModel = {
     find,
     findById,
-    Creat
+    Creat,
+    Update
 }
 module.exports = ProductsModel;
