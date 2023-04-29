20fbc23f88cb1b58e2c539df38666c3aa4582cef
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-29 15:06:47 +0330

    singleton

---
+++ utils/mongoconnection.js
@@ -0,0 +1,38 @@
+const {MongoClient} = require('mongodb');
+
+class ConnectToMongoDB{
+    #DB_URI = 'mongodb://127.0.0.1/mongodb-tutorials';
+    #db = null;
+    async #connect(){
+        try{
+            let client = new MongoClient(this.#DB_URI);
+            let db = client.db();
+            return db;
+        } catch(error){
+            console.log(error.message);
+        }
+    }
+
+    async Get(){
+        try{
+            if(this.#db){
+                console.log('DB connection is already alive');
+                return this.#db;
+            }
+            this.#db = await this.#connect();
+            return this.#db;
+
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+}
+
+async function main(){
+    const db = await new ConnectToMongoDB().Get();
+    const users = await db.collection('user').find({}).toArray();
+    console.log(users);
+}
+
+main();
+
